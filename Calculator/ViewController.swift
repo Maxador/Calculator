cd8a66662949a24a346f8674683dde2550150125
Maxador <mongeaumax@gmail.com>
2015-03-16 22:12:46 -0400

    Add basic view and calculation - Need to apply MVC to use a model instead of a stack in the controller

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text! = "\(newValue)"
+            isTypingNewNumber = false
+        }
+    }
+    
+    var isTypingNewNumber = false
+    var operandStack = [Double]()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isTypingNewNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isTypingNewNumber = true
+        }
+    }
+    
+    
+    @IBAction func enter() {
+        isTypingNewNumber = false
+        operandStack.append(displayValue)
+        println("operanStack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isTypingNewNumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation {$1 * $0}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$1 + $0}
+        case "−": performOperation {$1 - $0}
+        case "√": performOperation {sqrt($0)}
+        default : break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
